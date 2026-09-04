d2554d8e2e71dc3a3a90a6fe5bdc5edf36afa392
trumpercharles <trumper.charles@student.ccm.edu>
2026-09-04 14:32:38 -0400

    Completed lab 2, moving on to AI Report.

--- SalesBarChart.java
+++ SalesBarChart.java
@@ -1,5 +1,21 @@
 package Labs.Week_1.scr;
 
 public class SalesBarChart {
-    
+    public static void main(String args[]){
+        for (int c=1;c<=10;c+=1) {
+            for (int v=1;v<=c;v+=1){
+                System.out.print("*");
+            }
+
+            System.out.print("\n");
+        }
+
+        for (int c=1;c<=10;c+=1) {
+            for (int v=10;v>=c;v-=1){
+                System.out.print("*");
+            }
+
+            System.out.print("\n");
+        }
+    }
 }
