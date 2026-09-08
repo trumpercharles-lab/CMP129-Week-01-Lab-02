9c03a35c4dd013b123ddb25701d70e15049d4ffa
trumpercharles <trumper.charles@student.ccm.edu>
2026-09-08 14:24:09 -0400

    almost Completed lab 2, moving on to AI Report.

--- AI-Use-Report.md
+++ AI-Use-Report.md
@@ -11,7 +11,7 @@
 
 Did you use an AI tool for this lab?
 
-- [ ] Yes
+- [X] Yes
 - [ ] No
 
 If yes, complete the sections below. If no, write “No AI tool was used” under Summary.
@@ -20,13 +20,13 @@ If yes, complete the sections below. If no, write “No AI tool was used” unde
 
 Examples: GitHub Copilot, ChatGPT, Microsoft Copilot, or another tool.
 
-Tool:
+Tool: GitHub Copilot
 
 ## Assistance Requested
 
 Describe what you asked the AI tool to help you understand or troubleshoot.
 
-Response:
+Response: I asked the AI how to make exponents in java, 
 
 ## How I Used the Assistance
 
--- Patterns.java
+++ Patterns.java
@@ -1,25 +1,19 @@
-package Labs.Week_1.scr;
-import java.util.Scanner;
-
-public class Patterns {
+public class SalesBarChart {
     public static void main(String args[]){
-        Scanner newScanner=new Scanner(System.in);
-
-        System.out.print("SALES BAR CHART\n(Each * = $100)\n\n");
-
-        for(int c=1;c<=5;c+=1){
-            double numbStars;
-
-            System.out.print("Enter today's sales for store "+c+": ");
-            numbStars=newScanner.nextDouble()/100;
-            System.out.print("\n");
-
-            for(int s=1;s<=numbStars;s+=1){
+        for (int c=1;c<=10;c+=1) {
+            for (int v=1;v<=c;v+=1){
                 System.out.print("*");
             }
 
             System.out.print("\n");
         }
 
+        for (int c=1;c<=10;c+=1) {
+            for (int v=10;v>=c;v-=1){
+                System.out.print("*");
+            }
+
+            System.out.print("\n");
+        }
     }
 }
